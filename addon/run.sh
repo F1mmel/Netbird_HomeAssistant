f7cfb97142a8e1d94f84b33ed3a959ff3f4c0cba
Finn <83457170+F1mmel@users.noreply.github.com>
2025-11-11 16:59:25 +0100

    Change proxy_pass from HTTPS to HTTP

--- addon/run.sh
+++ addon/run.sh
@@ -51,7 +51,7 @@ http {
         server_name localhost;
 
         location / {
-            proxy_pass https://$HA_IP:$HA_PORT;
+            proxy_pass http://$HA_IP:$HA_PORT;
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
             proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
